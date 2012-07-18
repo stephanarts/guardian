5e1ff2527f448c9b6c801aa4a0ae6974bb90cd18
Stephan Arts <stephan@xfce.org>
2012-07-18 04:20:27 +0200

    Use meaningfull debug output

--- plugins/syslog/syslog.c
+++ plugins/syslog/syslog.c
@@ -248,12 +248,12 @@ _plugin_engine_update_source (
             if (memcmp (cache_hash, cache_file_hash, 20) == 0)
             {
                 /* DEBUG */
-                printf("OK\n");
+                printf("OK: Hash matches\n");
             }
             else
             {
                 /* DEBUG */
-                printf("FAIL\n");
+                printf("FAIL: Hash does not match\n");
             }
         }
 
