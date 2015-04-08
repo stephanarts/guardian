456a82bd2a8be01d05a8dd3d9d1b6d047c320e3c
Stephan Arts <stephan@xfce.org>
2015-04-08 22:32:15 +0200

    Read password from stdin if not provided

--- guardian/shell.c
+++ guardian/shell.c
@@ -46,7 +46,8 @@
 #include <unistd.h>
 #include <string.h>
 
-#include <histedit.h>
+#include <termios.h>
+#include <ctype.h>
 
 #include <openssl/sha.h>
 
@@ -57,21 +58,59 @@
 
 static void
 parse (char **tokens, int n_tokens) {
+    char buf[256];
+    struct termios saved_attributes, hide_pasword;
+
     if (n_tokens == 0) {
         return;
     }
 
     /* CONNECT */
     if (strcmp(tokens[0], "connect") == 0) {
-        client_connect_pass(
-                "tcp://localhost:1234",
-                tokens[1],
-                tokens[2]);
+        switch (n_tokens) {
+            case 3:
+                client_connect_pass(
+                        "tcp://localhost:1234",
+                        tokens[1],
+                        tokens[2]);
+                break;
+            case 2:
+                tcgetattr (STDIN_FILENO, &saved_attributes);
+                hide_pasword = saved_attributes;
+                hide_pasword.c_lflag &= ~ECHO;
+
+                tcsetattr (STDIN_FILENO, TCSANOW, &hide_pasword);
+
+                fprintf (stdout, "Password: ");
+                if(fgets (buf, 256, stdin) != NULL)
+                {
+                    fprintf (stdout, "\n");
+                    int l = strlen(buf);
+                    if (buf[l-1] == '\n') {
+                        buf[l-1] = '\0';
+                    }
+
+                    tcsetattr (STDIN_FILENO, TCSANOW, &saved_attributes);
+
+                    client_connect_pass(
+                            "tcp://localhost:1234",
+                            tokens[1],
+                            buf);
+                } else {
+                    fprintf (stdout, "\n");
+                    tcsetattr (STDIN_FILENO, TCSANOW, &saved_attributes);
+                }
+                break;
+            default:
+                printf("Connect Error\n");
+        }
+        return;
     }
 
     /* DISCONNECT */
     if (strcmp(tokens[0], "disconnect") == 0) {
          
+        return;
     }
 
     if (strcmp(tokens[0], "help") == 0) {
@@ -80,10 +119,17 @@ parse (char **tokens, int n_tokens) {
             fprintf(stdout, "help       - Show this message\n");
         }
         if (n_tokens == 2) {
+            if (strcmp(tokens[1], "connect") == 0) {
+                fprintf(stdout, "connect <user> [password]\n");
+            }
 
         }
         return;
     }
+
+    /* ERROR */
+    fprintf(stdout, "Parse error\n");
+    return;
 }
 
 void
@@ -121,7 +167,6 @@ show_shell (int silent)
             char *token = strsep(&key, " \t");
             while(token != NULL) {
                 if (*token != '\0') {
-                    //printf("'%s'\n", token); 
                     tokens[i] = token;
                     ++i;
                 }
@@ -137,4 +182,5 @@ show_shell (int silent)
             break;
         }
     }
+
 }
